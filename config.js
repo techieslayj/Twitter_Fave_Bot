ccb6a362d52900c3a89b21041407a4d375a80177
AJ Iglesias <ajigle1995@yahoo.com>
2019-10-05 15:29:19 -0400

    Update config.js
    deleted my token credentials

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	consumer_key: 'rIlKZ2usfuhdKaZ2VsiVBIEww',
-	consumer_secret: 'UBIlJS1M4M2Xc7I96xjq5HUpG1N9jZRtQzeY86aPs6DCMZ3cgO',
-	access_token_key: '609659068-R33CkEo11lCICtdQZ3ihpVVQ5RmC1hvh9718Vrso',
-	access_token_secret: 'AZ6opvf1xJe8dd6WJWtgTUDUQS21qznuIjq18QsuFRR7N'
+	consumer_key: 'fill with your own API keys',
+	consumer_secret: 'fill with your own API keys',
+	access_token_key: 'fill with your own API keys',
+	access_token_secret: 'fill with your own API keys'
 }
